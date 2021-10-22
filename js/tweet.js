a9b0c12cff1b1e4d72a0909500ea6fd0caeee390
Jumanah Almajnouni <jumanahh@outlook.com>
2021-10-22 01:36:58 +0000

    Completed the distance and activity type methods must update the html using jquery to include the values and extract the top 3 activities

--- js/tweet.js
+++ js/tweet.js
@@ -6,7 +6,6 @@ class Tweet {
     }
     //returns either 'live_event', 'achievement', 'completed_event', or 'miscellaneous'
     get source() {
-        //TODO: identify whether the source is a live event, an achievement, a completed event, or miscellaneous.
         var current = this.text;
         if (current.startsWith("Just posted") || current.toLowerCase().includes("completed")) {
             return 'completed_event';
@@ -23,7 +22,6 @@ class Tweet {
     }
     //returns a boolean, whether the text includes any content written by the person tweeting.
     get written() {
-        //TODO: identify whether the tweet is written
         var current = this.text.split('https')[0];
         if (current.endsWith("@Runkeeper. Check it out! ")) {
             return false;
@@ -36,47 +34,51 @@ class Tweet {
         if (!this.written) {
             return "";
         }
-        //TODO: parse the written text from the tweet
         return this.text.split("http")[0];
     }
     get activityType() {
         if (this.source != 'completed_event') {
             return "unknown";
         }else{
+            var extractActivity;
             if (this.text.startsWith("Just completed a")) {
-                var extractActivity = this.text.split("Just completed a")[1].split(" ");
-
-                // console.log(parseFloat(x[1]))
-                // console.log(x[1]);
-                // console.log(x[2])
+                extractActivity = this.text.split("Just completed a")[1].split(" ");
                 return extractActivity[3];
             } else if (this.text.startsWith("Just posted a")) {
-                console.log("posted")
+                extractActivity = this.text.split("Just posted a")[1].split(" ");
+                if (!isNaN(parseFloat(extractActivity[1]))) {
+                    return extractActivity[3];
+                }else{return "";}
             } else if (this.text.startsWith("Completed a")) {
-
+                extractActivity = this.text.split("Completed a")[1].split(" ");
+                return extractActivity[3];
             }
         }
-        return "";
     }
     get distance() {
         if (this.source != 'completed_event') {
             return 0;
         }else
         {
-            if (tweet_array[i].text.startsWith("Just completed a")) {
-                var extractDistance = tweet_array[i].text.split("Just completed a")[1].split(" ");
-                var distance = extractDistance[2] === "km" ? parseFloat(extractDistance[1]) * 0.621371 : parseFloat(extractDistance[1]);
-                return distance;
+            var distance;
+            var extractDistance;
+            if (this.text.startsWith("Just completed a")) {
+                extractDistance = this.text.split("Just completed a")[1].split(" ");
+                distance = extractDistance[2] === "km" ? parseFloat(extractDistance[1]) * 0.621371 : parseFloat(extractDistance[1]);
+                return distance.toFixed(2);
             }
-            else if (tweet_array[i].text.startsWith("Just posted a")) {
-                console.log("posted")
-            } else if (tweet_array[i].text.startsWith("Completed a")) {
-                console.log("hi")
+            else if (this.text.startsWith("Just posted a")) {
+                extractDistance = this.text.split("Just posted a")[1].split(" ");
+                if (!isNaN(parseFloat(extractDistance[1]))) {
+                    distance = extractDistance[2] === "km" ? parseFloat(extractDistance[1]) * 0.621371 : parseFloat(extractDistance[1]);
+                    return distance.toFixed(2);
+                } else { return ""; }
+            } else if (this.text.startsWith("Completed a")) {
+                extractDistance = this.text.split("Completed a")[1].split(" ");
+                distance = extractDistance[2] === "km" ? parseFloat(extractDistance[1]) * 0.621371 : parseFloat(extractDistance[1]);
+                return distance.toFixed(2);
             }
-
         }
-        //TODO: prase the distance from the text of the tweet
-        return 0;
     }
     getHTMLTableRow(rowNumber) {
         //TODO: return a table row which summarizes the tweet with a clickable link to the RunKeeper activity
